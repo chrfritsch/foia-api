02ace1ec2af86e2b9367e1f6751de98a153322ea
Christian Fritsch <731161+chrfritsch@users.noreply.github.com>
2026-06-18 18:03:31 +0000

    Remove deprecations

--- docroot/modules/custom/foia_upload_xml/src/EventSubscriber/FoiaUploadXmlMigrationPostImportSubscriber.php
+++ docroot/modules/custom/foia_upload_xml/src/EventSubscriber/FoiaUploadXmlMigrationPostImportSubscriber.php
@@ -58,7 +58,7 @@ class FoiaUploadXmlMigrationPostImportSubscriber implements EventSubscriberInter
   /**
    * {@inheritdoc}
    */
-  public static function getSubscribedEvents() {
+  public static function getSubscribedEvents(): array {
     return [
       MigrateEvents::POST_IMPORT => ['onPostAgencyImport'],
     ];
